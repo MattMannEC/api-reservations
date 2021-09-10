95dfab28b8cc8bd1c57e978090d0371bfe5fb293
mattmannec <mattmannec@gmail.com>
2021-09-10 09:18:56 +0200

    Doctrine entities and migrations

---
+++ migrations/Version20210909201350.php
@@ -0,0 +1,33 @@
+<?php
+
+declare(strict_types=1);
+
+namespace DoctrineMigrations;
+
+use Doctrine\DBAL\Schema\Schema;
+use Doctrine\Migrations\AbstractMigration;
+
+/**
+ * Auto-generated Migration: Please modify to your needs!
+ */
+final class Version20210909201350 extends AbstractMigration
+{
+    public function getDescription(): string
+    {
+        return '';
+    }
+
+    public function up(Schema $schema): void
+    {
+        // this up() migration is auto-generated, please modify it to your needs
+        $this->addSql('CREATE TABLE reservation (id INT AUTO_INCREMENT NOT NULL, vendor_id INT NOT NULL, space_id INT NOT NULL, INDEX IDX_42C84955F603EE73 (vendor_id), INDEX IDX_42C8495523575340 (space_id), PRIMARY KEY(id)) DEFAULT CHARACTER SET utf8mb4 COLLATE `utf8mb4_unicode_ci` ENGINE = InnoDB');
+        $this->addSql('ALTER TABLE reservation ADD CONSTRAINT FK_42C84955F603EE73 FOREIGN KEY (vendor_id) REFERENCES vendor (id)');
+        $this->addSql('ALTER TABLE reservation ADD CONSTRAINT FK_42C8495523575340 FOREIGN KEY (space_id) REFERENCES space (id)');
+    }
+
+    public function down(Schema $schema): void
+    {
+        // this down() migration is auto-generated, please modify it to your needs
+        $this->addSql('DROP TABLE reservation');
+    }
+}
---
+++ src/Entity/Reservation.php
@@ -0,0 +1,60 @@
+<?php
+
+namespace App\Entity;
+
+use App\Repository\ReservationRepository;
+use Doctrine\ORM\Mapping as ORM;
+
+/**
+ * @ORM\Entity(repositoryClass=ReservationRepository::class)
+ */
+class Reservation
+{
+    /**
+     * @ORM\Id
+     * @ORM\GeneratedValue
+     * @ORM\Column(type="integer")
+     */
+    private $id;
+
+    /**
+     * @ORM\ManyToOne(targetEntity=Vendor::class, inversedBy="reservations")
+     * @ORM\JoinColumn(nullable=false)
+     */
+    private $vendor;
+
+    /**
+     * @ORM\ManyToOne(targetEntity=Space::class, inversedBy="reservations")
+     * @ORM\JoinColumn(nullable=false)
+     */
+    private $space;
+
+    public function getId(): ?int
+    {
+        return $this->id;
+    }
+
+    public function getVendor(): ?Vendor
+    {
+        return $this->vendor;
+    }
+
+    public function setVendor(?Vendor $vendor): self
+    {
+        $this->vendor = $vendor;
+
+        return $this;
+    }
+
+    public function getSpace(): ?Space
+    {
+        return $this->space;
+    }
+
+    public function setSpace(?Space $space): self
+    {
+        $this->space = $space;
+
+        return $this;
+    }
+}
